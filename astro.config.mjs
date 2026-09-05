4909d37baaf04d112873f99a2c51b19fd7cf091d
Jai <jai@Jais-MacBook-Air.local>
2026-09-05 12:23:03 +0530

    Setup GitHub Pages deploy with base path

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://gogocabs.github.io',
+  base: '/kelvarix/',
   vite: {
     plugins: [tailwindcss()]
   }
